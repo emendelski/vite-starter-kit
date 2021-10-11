e42aafcf06c4edf077e3e353515f815a4be1005a
pmisiuk <pmisiuk@users.noreply.github.com>
2021-10-11 11:48:03 +0000

    :fire_engine: Apply formatting changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import path from 'path';
 import envCompatible from 'vite-plugin-env-compatible';
-import { injectHtml } from 'vite-plugin-html';
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import {injectHtml} from 'vite-plugin-html';
+import {viteCommonjs} from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,25 +11,13 @@ export default defineConfig({
     alias: [
       {
         find: '@',
-        replacement: path.resolve(__dirname, 'src')
-      }
+        replacement: path.resolve(__dirname, 'src'),
+      },
     ],
-    extensions: [
-      '.mjs',
-      '.js',
-      '.ts',
-      '.jsx',
-      '.tsx',
-      '.json',
-      '.vue'
-    ]
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
-  plugins: [
-    viteCommonjs(),
-    envCompatible(),
-    injectHtml()
-  ],
+  plugins: [viteCommonjs(), envCompatible(), injectHtml()],
   build: {
-    rollupOptions: {}
-  }
+    rollupOptions: {},
+  },
 });
