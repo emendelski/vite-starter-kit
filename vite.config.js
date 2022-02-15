36c6c1a35ed3eda224f3f4de254f21de952180fe
Kamil Mendelski <kamil@frontlabs.pl>
2022-02-15 10:16:54 +0100

    Add tilde alias for node_modules

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import {defineConfig} from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 import stylelintPlugin from '@frsource/vite-plugin-stylelint';
@@ -5,4 +7,12 @@ import stylelintPlugin from '@frsource/vite-plugin-stylelint';
 export default defineConfig({
   publicDir: 'static',
   plugins: [stylelintPlugin(), eslintPlugin()],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(__dirname, 'node_modules'),
+      },
+    ],
+  },
 });
