79c739dd0198f9964d3558337589e818a4a6a4c1
Kamil Mendelski <kamil@frontlabs.pl>
2024-04-08 16:14:32 +0200

    Autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import path from 'path';
 
 import {defineConfig} from 'vite';
+import autoprefixer from 'autoprefixer';
 import eslintPlugin from 'vite-plugin-eslint2';
 import stylelintPlugin from 'vite-plugin-stylelint';
 
 export default defineConfig({
   publicDir: 'static',
-  plugins: [stylelintPlugin(), eslintPlugin()],
+  plugins: [
+    eslintPlugin(),
+    stylelintPlugin(),
+  ],
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
   resolve: {
     alias: [
       {
